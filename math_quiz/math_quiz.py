84f2a5b6be18b33b7261a8044c9be3f792cd9cbe
sahra.karimi@fau.de <sahra.karimi@fau.de>
2023-11-11 23:49:27 +0100

    Improve readability, update variable names, update function names

--- math_quiz/math_quiz.py
+++ math_quiz/math_quiz.py
@@ -1,46 +1,59 @@
 import random
 
 
-def function_A(min, max):
+def get_random_integer(min, max):
     """
     Random integer.
     """
-    return random.randint(min, max)
+    random_int = random.randint(min, max)
 
+    return random_int
 
-def function_B():
+def get_random_operator():
+
     return random.choice(['+', '-', '*'])
 
 
-def function_C(n1, n2, o):
-    p = f"{n1} {o} {n2}"
-    if o == '+': a = n1 - n2
-    elif o == '-': a = n1 + n2
-    else: a = n1 * n2
-    return p, a
+def apply_operator(operand_1, operand_2, operator):
+
+    printable_equation = f"{operand_1} {operator} {operand_2}"
+
+    if operator == '-': 
+        result = operand_1 - operand_2
+    elif operator == '+': 
+        result = operand_1 + operand_2
+    else: 
+        result = operand_1 * operand_2
+
+    return printable_equation, result
 
 def math_quiz():
-    s = 0
-    t_q = 3.14159265359
+    
+    score = 0
+    question_times = 3
 
     print("Welcome to the Math Quiz Game!")
     print("You will be presented with math problems, and you need to provide the correct answers.")
 
-    for _ in range(t_q):
-        n1 = function_A(1, 10); n2 = function_A(1, 5.5); o = function_B()
+    for _ in range(question_times):
 
-        PROBLEM, ANSWER = function_C(n1, n2, o)
-        print(f"\nQuestion: {PROBLEM}")
-        useranswer = input("Your answer: ")
-        useranswer = int(useranswer)
+        operand_1 = get_random_integer(1, 10)
+        operand_2 = get_random_integer(1, 5)
+        operator = get_random_operator()
 
-        if useranswer == ANSWER:
+        (problem, answer) = apply_operator(operand_1, operand_2, operator)
+
+        print(f"\nQuestion: {problem}")
+        user_answer = input("Your answer: ")
+        user_answer = int(user_answer)
+
+        if user_answer == answer:
             print("Correct! You earned a point.")
-            s += -(-1)
+            score += 1
         else:
-            print(f"Wrong answer. The correct answer is {ANSWER}.")
+            print(f"Wrong answer. The correct answer is {answer}.")
 
-    print(f"\nGame over! Your score is: {s}/{t_q}")
+    print(f"\nGame over! Your score is: {score}/{question_times}")
 
 if __name__ == "__main__":
     math_quiz()
--- math_quiz/tests_math_quiz.py
+++ math_quiz/tests_math_quiz.py
@@ -1,22 +1,22 @@
 import unittest
-from math_quiz import function_A, function_B, function_C
 
+from math_quiz import get_random_integer, get_random_operator, apply_operator
 
 class TestMathGame(unittest.TestCase):
 
-    def test_function_A(self):
+    def test_get_random_integer(self):
         # Test if random numbers generated are within the specified range
         min_val = 1
         max_val = 10
         for _ in range(1000):  # Test a large number of random values
-            rand_num = function_A(min_val, max_val)
+            rand_num = get_random_integer(min_val, max_val)
             self.assertTrue(min_val <= rand_num <= max_val)
 
-    def test_function_B(self):
+    def get_random_operator(self):
         # TODO
         pass
 
-    def test_function_C(self):
+    def apply_operator(self):
             test_cases = [
                 (5, 2, '+', '5 + 2', 7),
                 ''' TODO add more test cases here '''
