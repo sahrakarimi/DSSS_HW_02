0b2333d6530600f278395486174a45c7feb090e6
sahra.karimi@fau.de <sahra.karimi@fau.de>
2023-11-12 18:39:37 +0100

    Fix missing url in setup.py, use find_packages

--- setup.py
+++ setup.py
@@ -1,7 +1,9 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
     name='math_quiz',
     version='0.1',
-    packages=['math_quiz'],
+    packages=find_packages(),
+    install_requires=['numpy >= 1.11.1', 'matplotlib >= 1.5.1'],
+    url='https://github.com/sahrakarimi/DSSS_HW_02.git'
 )
