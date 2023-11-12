8ad74a1b745c60bf16cd42e85fb97638394eddfa
sahra.karimi@fau.de <sahra.karimi@fau.de>
2023-11-12 12:12:15 +0100

    Improve the code in terms of comments and docstrings

--- math_quiz/math_quiz.py
+++ math_quiz/math_quiz.py
@@ -1,23 +1,49 @@
 import random
 
-
 def get_random_integer(min, max):
     """
-    Random integer.
+    Produce a random integer number in range min and max.
+
+    :param min: The min value 
+    :type min: int
+    :param max: The max value
+    :type max: int
+    :return: A random integer number between min and max
+    :rtype: int
     """
     random_int = random.randint(min, max)
 
     return random_int
 
 def get_random_operator():
+    """
+    Produce a random arithmetic operator from (+ , -, *).
 
-    return random.choice(['+', '-', '*'])
+    :return: A random arithmetic operator (addition, subtraction, multiplication)
+    :rtype: str (length-1)
+    """
+    random_operator = random.choice(['+', '-', '*'])
+
+    return random_operator
 
 
 def apply_operator(operand_1, operand_2, operator):
+    """
+    Calculate the arithmetic operation of two integer numbers.
+
+    :param operand_1: The first number
+    :type operand_1: int
+    :param operand_2: The second number
+    :type operand_2: int
+    :param operator: The random arithmetic operator
+    :type operator: str
+    :return: The equation string and result of arithmetic operation 
+    :rtype: tuple (str, int)
+    """
 
     printable_equation = f"{operand_1} {operator} {operand_2}"
 
+    # Calculate the result depending on the operator
     if operator == '-': 
         result = operand_1 - operand_2
     elif operator == '+': 
@@ -28,13 +54,18 @@ def apply_operator(operand_1, operand_2, operator):
     return printable_equation, result
 
 def math_quiz():
-    
+    """
+    It is a math game and ask the user a random math question for 
+    3-times and prints the calculated final score.
+    """
+
     score = 0
-    question_times = 3
+    question_times = 3    # TODO: make this as a function argument
 
     print("Welcome to the Math Quiz Game!")
     print("You will be presented with math problems, and you need to provide the correct answers.")
 
+    # Repeat the game for 'question_times' times
     for _ in range(question_times):
 
         operand_1 = get_random_integer(1, 10)
@@ -43,10 +74,12 @@ def math_quiz():
 
         (problem, answer) = apply_operator(operand_1, operand_2, operator)
 
+        # Print the question and wait to receive response from the user
         print(f"\nQuestion: {problem}")
         user_answer = input("Your answer: ")
         user_answer = int(user_answer)
 
+        # Validate the answer and update the score
         if user_answer == answer:
             print("Correct! You earned a point.")
             score += 1
--- math_quiz/tests_math_quiz.py
+++ math_quiz/tests_math_quiz.py
@@ -3,12 +3,23 @@ import unittest
 from math_quiz import get_random_integer, get_random_operator, apply_operator
 
 class TestMathGame(unittest.TestCase):
+    """
+    It is a test for math game whether it is working properly.
+    This is an extension of the :class:`unittest.TestCase` class.
+    """
 
     def test_get_random_integer(self):
-        # Test if random numbers generated are within the specified range
+        """
+        Test if random numbers generated are within the specified range
+        """
+       
         min_val = 1
         max_val = 10
-        for _ in range(1000):  # Test a large number of random values
+
+        # Test a large number of random values
+        number_of_samples = 1000
+
+        for _ in range(number_of_samples):  
             rand_num = get_random_integer(min_val, max_val)
             self.assertTrue(min_val <= rand_num <= max_val)
 
