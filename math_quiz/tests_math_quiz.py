71e6830553bc00326a0595dcc7462b25ff1c617a
sahra.karimi@fau.de <sahra.karimi@fau.de>
2023-11-12 19:43:06 +0100

    Add new test cases for math_quiz functions

--- math_quiz/tests_math_quiz.py
+++ math_quiz/tests_math_quiz.py
@@ -23,19 +23,36 @@ class TestMathGame(unittest.TestCase):
             rand_num = get_random_integer(min_val, max_val)
             self.assertTrue(min_val <= rand_num <= max_val)
 
-    def get_random_operator(self):
-        # TODO
-        pass
+    def test_get_random_operator(self):
+        """
+        Test if random operator generated are from ['+', '*', '-']
+        """
+        # Test a large number of times and check if it returns a valid operator
+        number_of_samples = 100
 
-    def apply_operator(self):
+        for _ in range(number_of_samples):  
+            operator = get_random_operator ()
+            self.assertTrue(operator in ['+', '*', '-'])
+
+    def test_apply_operator(self):
             test_cases = [
-                (5, 2, '+', '5 + 2', 7),
-                ''' TODO add more test cases here '''
+
+                (5, 2, '+', '5 + 2', 7),     
+                (4, -2, '+', '4 + -2', 2),
+                (-2,-3, '+', '-2 + -3', -5),
+                (3, 5, '*', '3 * 5', 15),
+                (-5, 4, '*', '-5 * 4', -20),
+                (-2, -3, '*', '-2 * -3', 6),
+                (10, 5, '-', '10 - 5', 5),
+                (2, -8, '-', '2 - -8', 10),
+                (-1,-2, '-', '-1 - -2', 1)
             ]
 
-            for num1, num2, operator, expected_problem, expected_answer in test_cases:
-                # TODO
-                pass
+            for num_1, num_2, operator, expected_problem, expected_answer in test_cases:
+
+                (result_equation, result) = apply_operator(num_1, num_2, operator)
+                self.assertEqual(result_equation, expected_problem)
+                self.assertEqual(result, expected_answer)
 
 if __name__ == "__main__":
     unittest.main()
