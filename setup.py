88cc27861d9d2e6925a27c5ccd00ab06370ac352
sahrakarimi <149941878+sahrakarimi@users.noreply.github.com>
2023-11-12 20:29:58 +0100

    Update setup.py and remove unnecessary packages.

--- setup.py
+++ setup.py
@@ -4,6 +4,6 @@ setup(
     name='math_quiz',
     version='0.1',
     packages=find_packages(),
-    install_requires=['numpy >= 1.11.1', 'matplotlib >= 1.5.1'],
+    install_requires=[],
     url='https://github.com/sahrakarimi/DSSS_HW_02.git'
 )
